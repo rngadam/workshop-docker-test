f693f60739b54e31785e65a633b1c966c590f561
andiechu <angdichu@gmail.com>
2019-06-29 16:06:42 +0800

    change

--- run.py
+++ run.py
@@ -3,5 +3,8 @@
 def execute(param):
     return param + 1
 
+def foo(param): 
+    return param * 2
+
 if __name__ == "__main__":
     print(execute(1))
--- run_test.py
+++ run_test.py
@@ -7,5 +7,8 @@ class TestRun(unittest.TestCase):
     def test_execute(self):
       self.assertEqual(2, run.execute(1))
 
+    def test_foo(self):
+      self.assertEqual(4, run.foo(2))
+
 if __name__ == '__main__':
     unittest.main()
