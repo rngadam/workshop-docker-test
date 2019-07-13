26763379a25717159ceffe6bfaa257da2d9ba15c
Ricky Ng-Adam <rngadam@gmail.com>
2019-07-13 13:37:33 +0800

    exception

--- run.py
+++ run.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 
+class RunException(Exception):
+    pass
+
+class RunExceptionInvalidParameter(RunException):
+    pass
+
 def execute(param):
-    return param + 1
+    try:
+        return param + 1
+    except TypeError:
+        raise RunExceptionInvalidParameter('invalid parameter')
 
 if __name__ == "__main__":
     print(execute(1))
--- run_test.py
+++ run_test.py
@@ -7,5 +7,16 @@ class TestRun(unittest.TestCase):
     def test_execute(self):
       self.assertEqual(2, run.execute(1))
 
+    def test_execute_10(self):
+      self.assertEqual(11, run.execute(10))
+
+    def test_execute_minus(self):
+      self.assertEqual(0, run.execute(-1))
+
+
+    def test_execute_invalid_parameter(self):
+        with self.assertRaises(run.RunExceptionInvalidParameter):
+            run.execute('')
+
 if __name__ == '__main__':
     unittest.main()
